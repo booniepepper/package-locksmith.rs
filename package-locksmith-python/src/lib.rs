14c0f06bc58007a7f55f0e5ec9bd7a6df5076bf0
Justin Hill <justin@so.dang.cool>
2025-07-30 23:10:30 -0700

    align package naming

--- package-locksmith-python/src/lib.rs
+++ package-locksmith-python/src/lib.rs
@@ -1,9 +1,10 @@
-pub mod poetry_lock;
 pub mod pylock_toml;
-pub mod requirements_txt;
 pub mod uv_lock;
-
-// pub use poetry_lock::PoetryLock;
 pub use pylock_toml::PylockToml;
-// pub use requirements_txt::RequirementsTxt;
 pub use uv_lock::UvLock;
+
+// TODO
+// mod poetry_lock;
+// mod requirements_txt;
+// pub use poetry_lock::PoetryLock;
+// pub use requirements_txt::RequirementsTxt;
--- package-locksmith/src/lib.rs
+++ package-locksmith/src/lib.rs
@@ -1,3 +1,6 @@
 pub use package_locksmith_java as java;
+pub use package_locksmith_python as python;
 
 pub use java::GradleLock;
+pub use python::PylockToml;
+pub use python::UvLock;
